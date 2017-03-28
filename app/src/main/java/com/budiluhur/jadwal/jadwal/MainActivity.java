06dacc954ba4509028ecd50eb4be83c11a88399a
newjar <newjar@newjar.local>
2017-03-28 19:38:02 +0700

    v1.0 release

---
+++ app/src/main/java/com/budiluhur/jadwal/jadwal/MainActivity.java
@@ -0,0 +1,97 @@
+package com.budiluhur.jadwal.jadwal;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        if(savedInstanceState != null) {
+            webView.restoreState(savedInstanceState);
+        } else {
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setSupportZoom(true);
+            webView.getSettings().setBuiltInZoomControls(true);
+            webView.getSettings().setLoadWithOverviewMode(true);
+            webView.getSettings().setUseWideViewPort(true);
+            webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+            webView.setWebViewClient(new OurViewClient());
+
+            webView.setWebChromeClient(new WebChromeClient() {
+                public void onProgressChanged(WebView view, int progress) {
+                    progressBar.setProgress(progress);
+                    if(progress < 100 && progressBar.getVisibility() == ProgressBar.GONE) {
+                        progressBar.setVisibility(ProgressBar.VISIBLE);
+                    }
+                    if (progress == 100) {
+                        progressBar.setVisibility((ProgressBar.GONE));
+                    }
+                }
+            });
+
+            String data = getIntent().getDataString();
+
+            if(Intent.ACTION_VIEW.equals(getIntent())) {
+                webView.loadUrl(data);
+            } else {
+                webView.loadUrl("http://jadwal.budiluhur.ac.id/");
+            }
+        }
+    }
+
+    public class OurViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView v, String url) {
+            if (url.contains("jadwal.budiluhur.ac.id")) {
+                v.loadUrl(url);
+                CookieManager.getInstance().setAcceptCookie(true);
+            } else {
+                Uri uri = Uri.parse(url);
+                startActivity(Intent.createChooser(new Intent(Intent.ACTION_VIEW, uri), "Choose browser"));
+            }
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webView.saveState(outState);
+    }
+}
